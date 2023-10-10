044ab66bc06e11703617946eaa48090ebdc32efa
Pushpinder Singh <142683189+Sohna-AI@users.noreply.github.com>
2023-10-10 08:46:38 -0700

    solved 13

--- problems/13-min-value-callback.js
+++ problems/13-min-value-callback.js
@@ -11,12 +11,16 @@ console.log(minValueCallback([64, 25, 49, 9, 100], Math.sqrt));  // 3
 *******************************************************************************/
 
 function minValueCallback(array, cb) {
-    // Your code here 
+  let small = Math.min(...array);
+  if (cb) return cb(small);
+  return small;
 }
 
+console.log(minValueCallback([64, 25, 49, 9, 100])); // 9
+console.log(minValueCallback([64, 25, 49, 9, 100], Math.sqrt)); // 3
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {
   module.exports = minValueCallback;
-} catch(e) {
+} catch (e) {
   return null;
 }
