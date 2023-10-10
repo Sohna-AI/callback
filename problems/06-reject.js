4c059b4a3b974580f31a4deec912473cdf1a31ec
Pushpinder Singh <142683189+Sohna-AI@users.noreply.github.com>
2023-10-09 21:09:20 -0700

    solved 06

--- problems/06-reject.js
+++ problems/06-reject.js
@@ -22,12 +22,23 @@ console.log(reject(['breadth', 'GRAPH', 'depth', 'height'], hasA)); // [ 'depth'
 *******************************************************************************/
 
 function reject(array, cb) {
-  // Your code here 
+  let result = [];
+  for (let i = 0; i < array.length; i++) {
+    let el = array[i];
+    let final = cb(el);
+    if (final === false) result.push(el);
+  }
+  return result;
 }
+
+let isEven = function (n) {
+  return n % 2 === 0;
+};
+console.log(reject([7, 8, 5, 6, 12, 11], isEven)); // [ 7, 5, 11 ]
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {
   module.exports = reject;
-} catch(e) {
+} catch (e) {
   return null;
 }
