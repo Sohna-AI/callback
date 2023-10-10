963b6ec899ff25569c77e5020c8acaa1cf5c51b9
Pushpinder Singh <142683189+Sohna-AI@users.noreply.github.com>
2023-10-10 09:39:53 -0700

    solved 17

--- problems/17-xor-select.js
+++ problems/17-xor-select.js
@@ -31,13 +31,44 @@ console.log(
 // [ 'art', 'app', 'buttery' ]
 *******************************************************************************/
 
-let xorSelect = function(array, cb1, cb2) {
-  // Your code here 
+let xorSelect = function (array, cb1, cb2) {
+  let result = [];
+  for (let i = 0; i < array.length; i++) {
+    let val = array[i];
+    let call1 = cb1(val);
+    let call2 = cb2(val);
+    if (call1 === true && call2 !== true) result.push(val);
+    if (call1 !== true && call2 === true) result.push(val);
+  }
+  return result;
 };
 
+// let isEven = function(n) {
+//   return n % 2 === 0;
+// };
+
+// let isPositive = function(n) {
+//   return n > 0;
+// };
+
+// console.log(xorSelect([-2, -1, 1, 2, 3, 4], isEven, isPositive));
+// // [ -2, 1, 3 ]
+
+let longString = function (s) {
+  return s.length > 4;
+};
+
+let startsA = function (s) {
+  return s[0] === "a";
+};
+
+console.log(
+  xorSelect(["art", "academy", "app", "cat", "buttery"], longString, startsA)
+);
+// [ 'art', 'app', 'buttery' ]
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {
   module.exports = xorSelect;
-} catch(e) {
+} catch (e) {
   return null;
 }
